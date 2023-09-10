689027e027f45868841262322f99a472d7ce4b4a
name-is-minh <name_is_minh@Minnie>
2023-09-09 22:30:34 -0500

    Update HTML

--- MenuChatbot/MenuChatbot/views.py
+++ MenuChatbot/MenuChatbot/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
 
 def home(request):
     return render(request, 'home.html')
+
+
+def submit(request):
+    messages = request.POST.getlist("messages[]")
+
+    print(messages)
+
+    data = {"message": messages}
+    return JsonResponse(data)
